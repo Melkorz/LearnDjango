fd156e92475d5c127d67f0afaff53b0401225997
Artyom <djeday2000a@yandex.ru>
2022-06-21 16:57:19 +0500

    Создание Django-проекта Команда: django-admin startproject project

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dilwofwnna%ymiqec+my9ry7sfqi54iv@g!hq(-1q1@%rgh1-9'
+SECRET_KEY = 'django-insecure-1u6yi1&^+(zc0h8!sv9c(8__exsil9)b3numr@2*qf-6_^$6a3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
