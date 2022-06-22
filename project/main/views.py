5770ec9061d89cad8a441e9494cc7177211136f4
Artyom <djeday2000a@yandex.ru>
2022-06-22 17:44:29 +0500

    .../project/urls.py + views.py Создание функции index + about

--- project/main/views.py
+++ project/main/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index():
-    pass
+def index(request):
+    return HttpResponse("<h4>Проверка!!!</h4>")
+
+
+def about(request):
+    return HttpResponse("<h4>Про нас</h4>")
+
