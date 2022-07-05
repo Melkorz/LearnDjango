86518256694062ed87c085df3d4821d965ecbc94
Artyom <djeday2000a@yandex.ru>
2022-07-05 18:43:10 +0500

    Создание страницы для кнопки "Добавить запись". Файл для подключения модели базы данных для форм на странице для добавления записей

--- project/news/forms.py
+++ project/news/forms.py
@@ -1,5 +1,5 @@
 from .models import Articles
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
 
 class ArticlesForm(ModelForm):
@@ -7,3 +7,25 @@ class ArticlesForm(ModelForm):
         model = Articles
         fields = ['title', 'anons', 'full_text', 'date']
 
+        widgets = {
+            "title": TextInput(attrs={
+                # Берём из шаблона, где у нас прописано поле ввода
+                'class': 'form-control',
+                'placeholder': 'Название статьи'
+            }),
+            "anons": TextInput(attrs={
+                # Берём из шаблона, где у нас прописано поле ввода
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+            "full_text": Textarea(attrs={
+                # Берём из шаблона, где у нас прописано поле ввода
+                'class': 'form-control',
+                'placeholder': 'Текст статьи'
+            }),
+            "date": DateTimeInput(attrs={
+                # Берём из шаблона, где у нас прописано поле ввода
+                'class': 'form-control',
+                'placeholder': 'Дата публикации'
+            })
+        }
