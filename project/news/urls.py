841786b58ed4b6324f861dfc8071920e127306ea
Artyom <djeday2000a@yandex.ru>
2022-07-06 17:48:55 +0500

    URL-адрес для страницы изменения конкретной новости

--- project/news/urls.py
+++ project/news/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
     path('<int:pk>', views.NewDetailViews.as_view(), name='news-detail'),  # pk - первичный ключ
+    path('<int:pk>/update', views.NewsUpdateViews.as_view(), name='news-update'),
 ]
