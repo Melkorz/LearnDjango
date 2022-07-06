82400fdc4f538a7ecc094e6d353be7db281f4c2d
Artyom <djeday2000a@yandex.ru>
2022-07-06 16:17:07 +0500

    Создание представления для HTML-страницы для вывода каждой новости

--- project/news/views.py
+++ project/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 
 def news_home(request):
@@ -25,3 +26,9 @@ def create(request):
         'error': error
     }
     return render(request, 'news/create.html', data)
+
+
+class NewDetailViews(DetailView):
+    model = DetailView
+    template_name = 'news/details_view.html'
+    context_object_name = 'articles' # Переменная для ключа
