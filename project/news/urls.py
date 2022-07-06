26f5f6f7995ef37dafd816ff6edb324eef8acdc5
Artyom <djeday2000a@yandex.ru>
2022-07-06 16:27:55 +0500

    Шаблон для просмотра каждой отдельной новости

--- project/news/urls.py
+++ project/news/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
-    path('<int:pk>', views.NewDetailViews.as_view(), name='news-detail'), # pk - первичный ключ
+    path('<int:pk>', views.NewDetailViews.as_view(), name='news-detail'),  # pk - первичный ключ
 ]
