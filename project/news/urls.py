19bc98ceeb5d56ab401a72817315a54158d27252
Artyom <djeday2000a@yandex.ru>
2022-07-06 16:21:02 +0500

    URL-адрес для файла каждой новости

--- project/news/urls.py
+++ project/news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
+    path('<int:pk>', views.NewDetailViews.as_view(), name='news-detail'), # pk - первичный ключ
 ]
